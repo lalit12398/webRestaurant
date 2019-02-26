13ef460dd03aef4241082cbc56054243609f1a13
lalit <chauhan.lalit95@gmail.com>
2019-02-26 13:02:21 +0530

    adding components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Simple Recipe', 'https://myfoodstory.com/wp-content/uploads/2017/02/Oven-Baked-Tandoori-Paneer-Tikka-2.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
